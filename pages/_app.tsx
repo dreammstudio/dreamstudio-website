0f5eb0c375759383856c011cb67231017d0b024d
dream <eneskamci10@gmail.com>
2022-01-10 22:36:59 +0300

    Delete Module

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import "react-loading-skeleton/dist/skeleton.css";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
