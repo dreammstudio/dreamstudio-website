a9628bdd02dbeaf0c46ee54548f9d1126b85feae
dream <eneskamci10@gmail.com>
2022-01-10 22:17:16 +0300

    Icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,12 +9,12 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>dream's website</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/dream" />
         <meta name="og:title" content="dream's website" />
           <meta name="og:type" content="website" />
           <meta name="og:site_name" content="dreamstudio.me" />
           <meta name="og:description" content="dream#3387's portfolio" />
-          <meta name="og:image" content="/vercel.svg" />
+          <meta name="og:image" content="/images/ogimage" />
       </Head>
   <Component {...pageProps} />
   </>
